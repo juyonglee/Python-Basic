dffba4e2c25b3f70a13fbfd4c6bcdeb33a68024a
JuYong Lee <juyonglee0208@gmail.com>
2016-12-13 18:56:39 +0900

    변수 설명 및 예제

---
+++ Chapter01. 데이터 다루기(수와 텍스트와 비트)/01_Variable.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+
+# Python은 프로그램 실행시에 자료형을 판단하는 
+# 동적 형식(Dynamic Typed Language) 언어
+# 자료형을 알고싶으면 type()함수를 이용
+
+var01 = 50
+print(type(var01))
+
+var02 = 50.0
+print(type(var02))
+
+var03 = "Hello World"
+print(type(var03))
