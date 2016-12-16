cf6ab949c00a40ea7a656612b1607a870269ee12
JuYong Lee <juyonglee0208@gmail.com>
2016-12-17 02:48:25 +0900

    함수 정의 설명 및 예제

---
+++ Chapter04. 함수로 코드 간추리기/01_FunctionDef.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# 파이썬에서 함수나 메소드를 정의할 때 definition을 줄인 Keyword인 def를 사용합니다.
+# def 함수이름 (매개변수 목록): 
+#     Code Block
+#     return 결과
+def welcomeJYL():
+    print "Hello! My name is JuYong Lee."
+welcomeJYL()
